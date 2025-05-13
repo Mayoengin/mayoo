64a15d5e72150ad0edd5f5e7abbd1658fc484389
Mayo.engin <mayo.engin@gmail.com>
2025-05-13 13:37:03 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-V6KJ5FZX.mjs";import"./chunk-5A3KTSQU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-UWI346YH.mjs";import"./chunk-5A3KTSQU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
